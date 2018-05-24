4d539d8d0b1e2325bc7bb39e470928c462299dd8
ZetaChow <zetachow@163.com>
2018-05-24 12:44:06 +0800

    上传，下载，删除

--- main.go
+++ main.go
@@ -33,13 +33,10 @@ func main() {
 
 	defer db.Close()
 
-	syncDownFile(db)
-
 	for {
 		syncDownFile(db) //同步远端有本地无的文件
-		syncUpFile(db)   //同步本端有远端无的文件
-		// xxxxxxxxxxxx //删除本地无远端有的文件
-		fmt.Println("ansyncFile over")
+		syncUpFile(db)   //同步本端有远端无的文件,删除本段有远端无的文件
+		fmt.Println("syncUpFile over")
 		time.Sleep(time.Second * config.Duration)
 	}
 
@@ -47,103 +44,87 @@ func main() {
 
 func syncUpFile(db *sql.DB) {
 
+	//将所有文件清单中的状态设置为0
+	db.Exec("UPDATE files SET exist=0 WHERE downloading=0")
+
 	//获取todo文件夹中的文件
 	localFiles := getLocalFiles(config.SyncFolder)
 
 	for _, f := range localFiles {
 
-		modTime, size, count, err := getFileStoreInfo(db, f.PATH)
+		modTime, size, count, downloading, err := getFileStoreInfo(db, f.PATH)
 		if err != nil {
-			fmt.Println(err.Error())
+			fmt.Println("getFileStoreInfo: ", err.Error())
 			continue
 		}
 
 		fmt.Printf("name:%s ; count:%d ; modTime: %s ;size:%d ", f.PATH, count, f.ModTime.Format("2006/01/02@15:04:05"), f.Size)
 
-		if count == 0 { //文件不存在
+		if count == 0 { //文件不存在于文件清单中
 
 			if _, err = upload(f.PATH, f.PATH); err != nil {
 				log.Println("上传错误", err.Error())
 				continue
 			}
 
-			if _, err = db.Exec("INSERT INTO files (fileName,modTime,size) VALUES (?,?,?)", f.PATH, f.ModTime, f.Size); err != nil {
+			if _, err = db.Exec("INSERT INTO files (fileName,modTime,size,exist) VALUES (?,?,?,1)", f.PATH, f.ModTime, f.Size); err != nil {
 				log.Println("更新数据错误", err.Error())
 			}
 
 			fmt.Println("添加到数据库中")
 
-		} else if modTime.UnixNano() != f.ModTime.UnixNano() || size != f.Size { //文件修改时间或尺寸不一致
+		} else if (modTime.UnixNano() != f.ModTime.UnixNano() || size != f.Size) && downloading == 0 { //文件修改时间或尺寸不一致
 
 			if _, err = upload(f.PATH, f.PATH); err != nil {
 				log.Println("上传错误", err.Error())
 				continue
 			}
 
-			if _, err = db.Exec("UPDATE files SET modTime=?,size=? WHERE fileName=?", f.ModTime, f.Size, f.PATH); err != nil {
+			if _, err = db.Exec("UPDATE files SET modTime=?,size=?,exist=1 WHERE fileName=?", f.ModTime, f.Size, f.PATH); err != nil {
 				log.Println("更新数据错误", err.Error())
 			}
 
 			fmt.Println("更新到数据库中")
 		} else {
+			//将文件存在状态改为1
+			db.Exec("UPDATE files SET exist=1 WHERE fileName=?", f.PATH)
+
 			fmt.Println("最新，无需同步")
 			continue
 		}
 
 	}
+
+	//删除所有 exist=0的文件
+	delNotExistFiles(db)
 }
 
 func syncDownFile(db *sql.DB) {
 	remoteFiles := getRemoteFileList()
 
 	for _, rf := range remoteFiles {
-		_, _, count, err := getFileStoreInfo(db, rf.Key)
+		_, _, count, _, err := getFileStoreInfo(db, rf.Key)
 		if err != nil {
-			fmt.Println(err.Error())
+			fmt.Println("download getFileStoreInfo", err.Error())
 			continue
 		}
 
 		if count == 0 { //清单中不存在此文件，
-			//下载到本地，并且记录到清单中
-			fmt.Println("下载文件...", rf.Key)
-			//获取URL，下载
-			dURL := getDownloadURL(rf.Key)
-			res, err := http.Get(dURL)
-			if err != nil {
-				log.Println("download error", err.Error())
-				continue
-			}
-
-			//创建目录
-			if err = os.MkdirAll(filepath.Dir(rf.Key), 0755); err != nil {
-				log.Println("create folder error", err.Error())
-				continue
-			}
-
-			//创建文件
-			file, err := os.Create(rf.Key)
-			if err != nil {
-				log.Println("create file error", err.Error())
-				continue
-			}
-
-			//写入到文件
-			_, err = io.Copy(file, res.Body)
-			if err != nil {
-				log.Println("write file error", err.Error())
-				continue
-			}
-
-			defer file.Close()
-
-			fileInfo, err := file.Stat()
-			if err != nil {
-				log.Println("get file info err:", err.Error())
-				continue
-			}
 
 			//记录到文件清单中
-			if _, err = db.Exec("INSERT INTO files (fileName,modTime,size) VALUES (?,?,?)", rf.Key, fileInfo.ModTime(), fileInfo.Size()); err != nil {
+			if _, err = db.Exec("INSERT INTO files (fileName,modTime,exist,downloading) VALUES (?,?,1,1)", rf.Key, time.Now()); err != nil {
+				log.Println("更新数据错误", err.Error())
+				continue
+			}
+
+			fileInfo, err := downloadFile(rf.Key)
+			if err != nil {
+				//如果下载文件出错，回退文件清单
+				db.Exec("DELETE FROM files WHERE fileName=?", rf.Key)
+			}
+
+			//更新到文件清单中
+			if _, err = db.Exec("UPDATE files SET downloading=0,modTime=?,size=? WHERE fileName=?", fileInfo.ModTime(), fileInfo.Size(), rf.Key); err != nil {
 				log.Println("更新数据错误", err.Error())
 			}
 
@@ -153,18 +134,89 @@ func syncDownFile(db *sql.DB) {
 	}
 }
 
-func getFileStoreInfo(db *sql.DB, fileName string) (modTime time.Time, size int64, count int, err error) {
-	rows, err := db.Query("SELECT modTime,size FROM files WHERE fileName=?", fileName)
+func downloadFile(fileName string) (fileInfo os.FileInfo, err error) {
+	//下载到本地，并且记录到清单中
+	fmt.Println("下载文件...", fileName)
+	//获取URL，下载
+	dURL := getDownloadURL(fileName)
+	res, err := http.Get(dURL)
+	if err != nil {
+		log.Println("download error", err.Error())
+		return
+	}
+
+	//创建目录
+	if err = os.MkdirAll(filepath.Dir(fileName), 0755); err != nil {
+		log.Println("create folder error", err.Error())
+		return
+	}
+
+	//创建文件
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Println("create file error", err.Error())
+		return
+	}
+	defer file.Close()
+
+	//写入到文件
+	_, err = io.Copy(file, res.Body)
+	if err != nil {
+		log.Println("write file error", err.Error())
+		return
+	}
+
+	return file.Stat()
+
+}
+
+func getFileStoreInfo(db *sql.DB, fileName string) (modTime time.Time, size int64, count, downloading int, err error) {
+
+	rows, err := db.Query("SELECT modTime,size,downloading FROM files WHERE fileName=?", fileName)
 	if err != nil {
 		return
 	}
 
 	for rows.Next() {
-		err = rows.Scan(&modTime, &size)
+		err = rows.Scan(&modTime, &size, &downloading)
 		if err != nil {
 			return
 		}
 		count++
+	}
+
+	return
+}
+
+func delNotExistFiles(db *sql.DB) {
+
+	rows, err := db.Query("SELECT fileName FROM files WHERE exist=0")
+	if err != nil {
+		log.Println("查询不存在的文件出错:", err.Error())
+		return
+	}
+
+	var fileName string
+	for rows.Next() {
+
+		err = rows.Scan(&fileName)
+		if err != nil {
+			log.Println("读取数据文件出错:", err.Error())
+			return
+		}
+
+		fmt.Println("file will del", fileName)
+		//删除远端文件
+		if err = delRemoteFile(fileName); err != nil {
+			log.Println("删除远端文件出错：", err.Error())
+		}
+
+	}
+
+	_, err = db.Exec("DELETE FROM files WHERE exist=0")
+	if err != nil {
+		log.Println("删除文件清单报错：", err.Error())
+		return
 	}
 
 	return
@@ -181,7 +233,9 @@ func createDB() (db *sql.DB, err error) {
     CREATE TABLE IF NOT EXISTS files(
         fileName VARCHAR(255) PRIMARY KEY,
 		modTime DATE NULL,
-		size INTEGER 
+		size INTEGER DEFAULT 0,
+		exist INTEGER DEFAULT 0,
+		downloading INTEGER DEFAULT 0
     );
 	`
 
@@ -248,8 +302,8 @@ func loadConfig() (err error) {
 func upload(key, localFile string) (ret storage.PutRet, err error) {
 
 	putPolicy := storage.PutPolicy{
-		Scope:    fmt.Sprintf("%s:%s", config.Bucket, key),
-		FileType: 1,
+		Scope: fmt.Sprintf("%s:%s", config.Bucket, key),
+		//FileType: 1, //1 低频存储
 	}
 	mac := qbox.NewMac(config.AccessKey, config.SecretKey)
 	upToken := putPolicy.UploadToken(mac)
@@ -321,4 +375,15 @@ func getRemoteFileList() (remoteFiles []storage.ListItem) {
 	}
 
 	return
+}
+
+func delRemoteFile(fileName string) (err error) {
+	mac := qbox.NewMac(config.AccessKey, config.SecretKey)
+	cfg := storage.Config{
+		// 是否使用https域名进行资源管理
+		UseHTTPS: false,
+	}
+	bucketManager := storage.NewBucketManager(mac, &cfg)
+
+	return bucketManager.Delete(config.Bucket, fileName)
 }
